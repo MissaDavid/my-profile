da0fc614aa630614572af1d8f2d33d0a74d1f5fc
Mel Tamplin <mei.chohee@gmail.com>
2023-12-28 15:52:53 -0500

    (footer): name and text update
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="mb-10 px-4 mt-16 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2023 Melissa Tamplin David
+        &copy; 2023 Melissa Tamplin
       </small>
       <p className="text-xs">
         This website was built by me, using React, Next.js, TypeScript, Tailwind CSS,
-        Framer Motion and Resend.
+        Framer Motion and Resend. Deployed with Vercel :)
       </p>
     </footer>
   );
